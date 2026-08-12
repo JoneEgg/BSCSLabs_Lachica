cd8bb225b2a1cc0317e0f982b87e93714b822e0a
Jonnelle Yvonn Me„Ez Lachica <Jonnellelachica@gmail.com>
2026-08-12 10:28:10 +0800

    1.3.2.2 Biography

---
+++ Lab2/page1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page 1</title>
+</head>
+<body>
+    <h1>NAVIGATION</h1>
+    This is Page 1
+    <ul>
+        <li><a href="index.php" target="_blank">Index</a></li>
+        <li><a href="page2.php" target="_blank">Page 2</a></li>
+        <li><a href="page3.php" target="_blank">Page3</a></li>
+    </ul>
+    
+</body>
+</html>
---
+++ Lab2/page3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>NAVIGATION</h1>
+    This is Page 3
+    <ul>
+        <li><a href="index.php" target="_blank">Index</a></li>
+        <li><a href="page1.php" target="_blank">Page 1</a></li>
+        <li><a href="page2.php" target="_blank">Page2</a></li>
+    </ul>
+</body>
+</html>
