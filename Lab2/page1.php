2a735c113d82af26481a650846e4bdbb92e42c8d
Jonnelle Lachica <a@Jones.local>
2026-08-16 01:49:51 +0800

    1.3.2.2 Biography_V2

--- Lab2/page1.php
+++ Lab2/page1.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page 1</title>
+    <title>About Me</title>
 </head>
-<body>
-    <h1>NAVIGATION</h1>
-    This is Page 1
-    <ul>
-        <li><a href="index.php" target="_blank">Index</a></li>
-        <li><a href="page2.php" target="_blank">Page 2</a></li>
-        <li><a href="page3.php" target="_blank">Page3</a></li>
-    </ul>
+<body style="padding:100px;text-align: center;">
+    <h1>ABOUT ME</h1>
+    <a href="index.php">Home</a> • <a href="page2.php">Illustrations</a> • <a href="page3.php">Graphic Designs</a>
+
+    <p style="margin-top: 30px;padding-left:300px;padding-right:300px">My name is <b>Jonnelle Yvonn M. Lachica</b>. In school I go by <b>Jon/Jun</b>, my older friends call me <b>Jones</b>. 
+    I'm currently taking the <b>Bachelors of Science in Computer Science</b> with the focus on <b>Game Development</b>.
+    As an <b>artist</b> first and foremost, I'm particularly attracted to the creative side of <b>game design</b> — writing, 
+    3D rendering, concept art, illustration, UI design etc. I've got so many ideas and so little time!</p>
     
 </body>
 </html>
--- Lab2/page3.php
+++ Lab2/page3.php
@@ -3,15 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Graphic Designs</title>
 </head>
-<body>
-    <h1>NAVIGATION</h1>
-    This is Page 3
-    <ul>
-        <li><a href="index.php" target="_blank">Index</a></li>
-        <li><a href="page1.php" target="_blank">Page 1</a></li>
-        <li><a href="page2.php" target="_blank">Page2</a></li>
-    </ul>
+<body style="padding:100px;text-align: center;">
+    <h1>GRAPHIC DESIGNS</h1>
+    <a href="index.php">Home</a> • <a href="page1.php">About Me</a> • <a href="page2.php">Illustrations</a>
+    <br>
+    <br>
+    <p style="margin-bottom:100px">A collection of my graphic design work. Mostly old pieces.</p>
+
+    <img src="./Portfolio/quandary.png" alt="Ticket design for GDG's Quandary, 2024" style="max-width: 100%;max-height: 100vh;margin: auto;">
+    <p style="margin-bottom:100px;font-size:large">Ticket design for GDG's escape room, made with Josh Dane Labistre, 2024</p>
+    <img src="./Portfolio/night mode.png" alt="Ad for GDG's escape room, 2024" style="max-width: 100%;max-height: 100vh;margin: auto;">
+    <p style="margin-bottom:100px;font-size:large">Poster for GDG's escape room, 2024</p>
+    <img src="./Portfolio/PosterL.png" alt="Seminar advertisement poster" style="max-width: 100%;max-height: 100vh;margin: auto;">
+    <p style="margin-bottom:100px;font-size:large">Poster design for a webinar hosted by my group, 2023</p>
+    
+    
 </body>
 </html>
